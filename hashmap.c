459d08f3f26b8832f4e98ccdcf78d3456fc531ac
Lilcrashpark <142242742+Lilcrashpark@users.noreply.github.com>
2024-04-24 15:04:53 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -41,7 +41,26 @@ int is_equal(void* key1, void* key2){
 
 void insertMap(HashMap * map, char * key, void * value)
 {
-  
+  if(map == NULL || key == NULL)
+  {
+    return;
+  }
+  if(map->size == map->capacity)
+  {
+    enlarge(map);
+  }
+  long posicion = hash(key,map->capacity);
+  while(map->buckets[posicion] != NULL && map->buckets[posicion]->key != NULL)
+    {
+      if(is_equal(map->buckets[posicion]->key,key))
+      {
+        map->buckets[posicion]->value = value;
+        return;
+      }
+      posicion = (posicion + 1) % map->capacity;
+    }
+  map->buckets[posicion] = createPair(key,value);
+  map->size++;
 }
 
 void enlarge(HashMap * map) {
