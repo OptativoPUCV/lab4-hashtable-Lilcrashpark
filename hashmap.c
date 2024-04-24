b0094d969d017256df7446b7b8042dc7e002463e
Lilcrashpark <142242742+Lilcrashpark@users.noreply.github.com>
2024-04-24 14:37:28 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -39,10 +39,7 @@ int is_equal(void* key1, void* key2){
 }
 
 
-void insertMap(HashMap * map, char * key, void * value) {
-
-
-}
+void insertMap(HashMap * map, char * key, void * value)
 
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
